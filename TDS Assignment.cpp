62f5d0f3185b9a26d4c17467a2fbfb4326e34ffe
gamino-acid <chin.foog.sin@student.mmu.edu.my>
2025-05-27 12:20:28 +0800

    Project

---
+++ TDS Assignment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int i;
+cout<<"Hello world"; 
+
+
+
+
+return 0;// The code above is a simple C++ program that prints "Hello world" to the console.
+}
